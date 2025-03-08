453f318b1b790b7929d3254f9c7294706f6dce53
Bhagyodaya_Varshney <mtrbhagyodayame@email.com>
2025-03-08 08:05:16 +0530

    UC-5 User Authentication and Authorization Implemented

--- src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -1,11 +1,14 @@
 package com.example.AddressBookApp;
 
+import com.example.AddressBookApp.controllers.UserController;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
 	}
