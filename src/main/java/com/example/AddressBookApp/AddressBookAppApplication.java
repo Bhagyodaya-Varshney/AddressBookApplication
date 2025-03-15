5464559ee8b021e2ee26791fad4bb0d03c5a86db
Bhagyodaya_Varshney <mtrbhagyodayame@email.com>
2025-03-15 17:26:35 +0530

    UC-8 Redis Implementation DONE

--- src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -1,9 +1,5 @@
 package com.example.AddressBookApp;
 
-import com.example.AddressBookApp.controllers.UserController;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/AddressBookApp/model/AddressBookModel.java
+++ src/main/java/com/example/AddressBookApp/model/AddressBookModel.java
@@ -2,7 +2,6 @@ package com.example.AddressBookApp.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.context.annotation.Bean;
 
 @Entity
 @Table(name = "AddressBook")
